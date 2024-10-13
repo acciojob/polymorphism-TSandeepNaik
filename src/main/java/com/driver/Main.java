9a4cc831cdaea1049c402a9b4c066425ffd330bb
TSandeepNaik <sandeepnaikat100@gmail.com>
2024-10-13 20:08:53 +0530

    polymorphism solved

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
+class Product{
 
+    public int product(int x, int y) {};
+    public int product(int x, int y, int z) {};
+    public double product(double x, double y) {};
+
+}
 public class Main {
-
+    public static void main(String[] args) {
+        Product p = new Product();
+    }
 }
