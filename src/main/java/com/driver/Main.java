5d1799f7daa05c453941b79c33a6e527b42fbbc0
TSandeepNaik <sandeepnaikat100@gmail.com>
2024-10-14 13:33:53 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,10 +1,14 @@
 package com.driver;
-class Product{
+public static class Product{
 
-    public int product(int x, int y) {};
-    public int product(int x, int y, int z) {};
+    public int product(int x, int y) {
+        return x+y;
+    }
+    public int product(int x, int y, int z) {
+        return x+y+z;
+    };
     public double product(double x, double y) {};
-
+          return  (double)x+y;
 }
 public class Main {
     public static void main(String[] args) {
